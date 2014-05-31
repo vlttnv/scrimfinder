fdef88fb59b37a3ea5ba4f5696205e9a7c7d539d
Charlito <cjw.charleswu@gmail.com>
2014-06-01 04:36:22 +0800

    Simple admin page accessible @ 'localhost:5000/admin'

--- scrim/__init__.py
+++ scrim/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
 from flask.ext.openid import OpenID
 
 scrim_app = Flask(__name__)
 scrim_app.config.from_pyfile('../config.py')
+
 db = SQLAlchemy(scrim_app)
 oid = OpenID(scrim_app)
+admin = Admin(scrim_app, name='Scrim Finder')
+
 from scrim import views, models
+from models import User
+
+admin.add_view(ModelView(User, db.session))
