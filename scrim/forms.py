717633e10c26a64b5ab5cef0732eb45f50529121
Valentin Tunev <hdbuster@gmail.com>
2014-06-02 17:14:41 +0200

    drops downs and some links

--- scrim/forms.py
+++ scrim/forms.py
@@ -1,7 +1,10 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
+from wtforms import TextField, BooleanField, SelectField
 from wtforms.validators import Required, Length
+import consts
 
 class EditForm(Form):
     team_skill_level    = TextField('team_skill_level', validators = [Required()])
+    team_skill_level    = SelectField(u'Skill Level', choices=[('UGC_PLATINUM', 'UGC Platinum'), ('UGC_GOLD', 'UGC Gold'), ('UGC_SILVER', 'UGC Silver')])
     team_time_zone      = TextField('team_time_zone', validators = [Required()])
+
