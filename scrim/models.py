71816e35c2ffa968d2bfc9f96d57f18b049a6706
Valentin Tunev <hdbuster@gmail.com>
2014-06-09 09:20:20 +0200

    Comments

--- scrim/models.py
+++ scrim/models.py
@@ -2,6 +2,12 @@ from datetime import datetime
 from scrim import db
 
 class User(db.Model):
+    """
+    The user model store information about single user
+    logged in through steam. Most of the information is set on first
+    log in.
+    """
+
     id                  = db.Column(db.Integer, primary_key=True)
     role                = db.Column(db.Integer)
     steam_id            = db.Column(db.String(40), unique=True)
@@ -35,12 +41,25 @@ class User(db.Model):
         return unicode(self.id)
 
 class Request(db.Model):
+    """
+    A request is made when a certain member wants to join a team.
+    There might be multiple requests per user for different teams.
+    (need to make checks for that).
+
+    There should be some kind of a limit to the number of requests
+    that a user can make.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-
 class Team(db.Model):
+    """
+    The team model store single team information. Reputation is an
+    incrementing value (write a method for it).
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     skill_level = db.Column(db.String(80))
@@ -51,12 +70,24 @@ class Team(db.Model):
     scrim_team1 = db.relationship('Scrim', foreign_keys='Scrim.team_id2', backref='team', lazy='dynamic')
 
 class Membership(db.Model):
+    """
+    A membership is made after a request is approved and deleted
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     role = db.Column(db.String(80))
 
 class Scrim(db.Model):
+    """
+    Scrim stores information about a proposed scrim, challenge
+    or finished scrim
+
+    to be added:
+    state of scrim - finished, pending, invalid, proposed, challenge...
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     start_time = db.Column(db.String(80))
     time_zone = db.Column(db.String(80))
