6d8871f310aab845360db41b1a61e4e79a9cbb73
Valentin Tunev <hdbuster@gmail.com>
2014-07-21 03:56:42 -0400

    Added a function to convert bits to days in templates

--- scrim/__init__.py
+++ scrim/__init__.py
@@ -22,6 +22,14 @@ admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Team, db.session))
 admin.add_view(ModelView(Scrim, db.session))
 
+@scrim_app.context_processor
+def utility_processor():
+    def convert_days(bit_string):
+        days_of_week = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+        aval = [day for day, bit in zip(days_of_week, bit_string) if bit == '1']
+        return aval
+    return dict(convert_days=convert_days)
+
 # Blueprints - Not Working
 # from scrim.teams.views import teams as teams_module
 # scrim_app.register_blueprint(teams_module, url_prefix='/teams')
