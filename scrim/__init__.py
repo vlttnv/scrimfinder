7c12d4e39ab2026badecdb3730a6ce2ada9484b1
Charlito <cjw.charleswu@gmail.com>
2014-07-24 20:09:49 +0100

    use scrim labels in /teams and /scrims

--- scrim/__init__.py
+++ scrim/__init__.py
@@ -15,14 +15,19 @@ lm.init_app(scrim_app)
 from scrim import views, models
 from models import User, Team, Scrim
 
-
 @scrim_app.context_processor
 def utility_processor():
     def convert_days(bit_string):
         days_of_week = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
         aval = [day for day, bit in zip(days_of_week, bit_string) if bit == '1']
         return aval
-    return dict(convert_days=convert_days)
+    def get_time_zone_label(time_zone):
+		from consts import TIME_ZONES_DICT
+		for item in TIME_ZONES_DICT:
+			if item['time_zone'] == time_zone:
+				return item['label']
+		return None
+    return dict(convert_days=convert_days, get_time_zone_label=get_time_zone_label)
 
 # Blueprints - Not Working
 # from scrim.teams.views import teams as teams_module
